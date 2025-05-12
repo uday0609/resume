aeb0dd9c307f030bbc79d7c57dded451301c2179
Your Name <you@example.com>
2025-05-12 23:30:26 +0530

    Remove all existing code

--- react_frontend_resume/resumeapp/src/components/layout1/index.jsx
+++
@@ -1,5 +0,0 @@
-import Header from "./Header";
-import Main from "./Main";
-import Sidebar from "./Sidebar";
-
-export { Sidebar, Main, Header };
--- react_frontend_resume/resumeapp/src/components/pages/Footer.jsx
+++
@@ -1,5 +0,0 @@
-export default function Footer(){
-    return (
-      <Footer>Hello </Footer>  
-    );
-}
